09338c0c54a3cd197f090238c70619c89b9a3d8a
Siddhant Jain <50648970+siddhantjain07@users.noreply.github.com>
2020-06-29 23:58:25 +0530

    Add files via upload

---
+++ Menger Sponge/Menger_Sponge_/Menger_Sponge_.js
@@ -0,0 +1,34 @@
+var a = 0;
+var sponge = [];
+
+function setup() {
+createCanvas(800,800,WEBGL);
+normalMaterial();
+var b = new Box(0,0,0,200);
+sponge.push(b);
+}
+
+function mousePressed() {
+var next = [];
+for (var i=0; i<sponge.length; i++){
+var b = sponge[i];
+var newBoxes = b.generate();
+next = next.concat(newBoxes);
+}
+sponge = next;
+}
+
+
+function draw() {
+background(21);
+stroke(255);
+noFill();
+lights();
+rotateX(a);
+rotateY(a*0.4);
+rotateZ(a*0.1);
+for(var i=0; i<sponge.length; i++){
+sponge[i].show();
+}
+a += 0.01;
+}
